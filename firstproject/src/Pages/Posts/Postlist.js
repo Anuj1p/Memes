4a2c043ae5c529a3cc002b7fb3ceecfbb594ec80
Anuj1p <anujjangra2002@gmail.com>
2021-08-30 01:32:28 +0530

    Change in CSS

--- firstproject/src/Pages/Posts/Postlist.js
+++ firstproject/src/Pages/Posts/Postlist.js
@@ -1,29 +1,42 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import axios from 'axios';
-import './Postlist.css';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import "./Postlist.css";
 
 const PostList = (props) => {
-
-
-    return (
-        <>
-            {props.posts.map((post) => (
-                <div className="card-cards" >
-                    <div className="card-body">
-                        <h5 className="card-title"> {post.name}</h5>
-                        <img src={post.data} alt="meme" />
-                        {/* <button className='btn btn-danger btn-group2' id={post._id} onClick={() => { axios.delete(`http://localhost:4000/app/feed/${post._id}`, window.location.reload()) }} >Delete</button> */}
-                    </div>
-                    <div className="button">
-                        <NavLink to = {`/feed/${post._id}`}><button className="btno"  >Edit</button></NavLink>
-                        <button className='btno' id={post._id} onClick={() => { axios.delete(`http://localhost:4000/app/feed/${post._id}`, window.location.reload()) }} >Delete</button>
-                    </div>
-                </div>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {props.posts.map((post) => (
+        <div className="card-cards">
+          <div className="card-body">
+            <div>
+              <h5 className="card-title"> {post.name}</h5>
+            </div>
+            <div>
+              <img src={post.data} alt="meme" />
+            </div>
+          </div>
+          <div className="button">
+            <button className="btno">
+              <NavLink to={`/feed/${post._id}`}>Edit</NavLink>
+            </button>
+            <button
+              className="btno"
+              id={post._id}
+              onClick={() => {
+                axios.delete(
+                  `http://localhost:4000/app/feed/${post._id}`,
+                  window.location.reload()
+                );
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
-export default PostList
+export default PostList;
